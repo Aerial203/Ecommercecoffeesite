1522c00de4f6b88a96957455fc6a30ee6fce6f2c
Ankit Shrestha <ankit.shrestha196@gmail.com>
2023-05-12 19:55:06 +0545

    Add Login route

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
